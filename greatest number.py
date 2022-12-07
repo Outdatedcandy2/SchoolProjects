e7dda291adaf5738701d812687abb8be90c59d44
Outdatedcandy2 <114146070+Outdatedcandy2@users.noreply.github.com>
2022-12-07 21:00:46 +0530

    ayo

--- greatest number.py
+++ greatest number.py
@@ -1,10 +1,13 @@
 x = int(input("Enter First Number:"))
 y = int(input("Enter Second Number:"))
+z = int(input("Enter Third Number:"))
 
-if x > y :
-    print(f"{x} is greater than {y}")
-elif y > x :
-    print(f"{y} is greater than {x}")
+if x > y and x > z :
+    print(f"{x} is greater than {y} and {z}")
+elif y > x and y > z :
+    print(f"{y} is greater than {x} and {z}")
+elif z > x and z > y :
+    print(f"{z} is greater than {x} and {y}")        
 else:
-    print("both are equal")    
+    print("Are All Equal")    
 
---
+++ table.py
@@ -0,0 +1,8 @@
+number = int(input("Enter Number : "))
+
+times = 1
+
+while times <=10:
+    product = times * number
+    print(f"{number} x {times} = {product}")
+    times = times + 1
